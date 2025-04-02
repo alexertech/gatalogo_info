589c8394b5c281bbc37ea791080a184831c5fe41
Alex B <ab@alexertech.com>
2025-04-02 16:44:04 -0300

    Specs where added

--- test/models/gato_test.rb
+++ test/models/gato_test.rb
@@ -1,7 +1,59 @@
 require "test_helper"
 
 class GatoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save gato without required fields" do
+    gato = Gato.new
+    assert_not gato.save, "Saved the gato without required fields"
+  end
+
+  test "should save valid gato" do
+    # Create a valid user for association
+    user = users(:one)
+    
+    # Create a valid gato
+    gato = Gato.new(
+      name: "Whiskers",
+      breed: "Siamese",
+      description: "A beautiful cat",
+      user: user
+    )
+    
+    assert gato.save, "Could not save valid gato"
+  end
+  
+  test "should validate presence of name" do
+    user = users(:one)
+    gato = Gato.new(
+      breed: "Siamese",
+      description: "A beautiful cat",
+      user: user
+    )
+    
+    assert_not gato.valid?
+    assert_includes gato.errors[:name], "can't be blank"
+  end
+  
+  test "should validate presence of breed" do
+    user = users(:one)
+    gato = Gato.new(
+      name: "Whiskers",
+      description: "A beautiful cat",
+      user: user
+    )
+    
+    assert_not gato.valid?
+    assert_includes gato.errors[:breed], "can't be blank"
+  end
+  
+  test "should validate presence of description" do
+    user = users(:one)
+    gato = Gato.new(
+      name: "Whiskers",
+      breed: "Siamese",
+      user: user
+    )
+    
+    assert_not gato.valid?
+    assert_includes gato.errors[:description], "can't be blank"
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,10 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+# Integration test helpers for Devise
+module ActionDispatch
+  class IntegrationTest
+    include Devise::Test::IntegrationHelpers
+  end
+end
