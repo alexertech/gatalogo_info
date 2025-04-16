625ab1421b32b170f5f3b3f96e1344c69afa82db
Alex B <ab@alexertech.com>
2025-04-15 20:27:57 -0400

    restoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules
   # Temporarily disabled :registerable to prevent new sign-ups
-  devise :database_authenticatable, 
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :gatos, dependent: :destroy
